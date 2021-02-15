fb95601e73acc34e3585f2b2c056e883eab0e658
xiso <xiso@amuz.co.kr>
2021-02-16 05:36:00 +0900

    사이트목록 위젯에 타겟인스턴스 설정 추가

--- components/Widgets/SiteList/SiteListWidget.php
+++ components/Widgets/SiteList/SiteListWidget.php
@@ -12,17 +12,29 @@ class SiteListWidget extends \Xpressengine\Widget\AbstractWidget
     {
         $widgetConfig = $this->setting();
         $title = $widgetConfig['@attributes']['title'];
-        $Sites = Site::limit(array_get($this->config, 'latest_count',10))->get();
+        $Sites = Site::whereDoesntHave('configMeta', function($query){
+            $query->where('vars','like','%"use_list":"N"%');
+        })->limit(array_get($this->config, 'latest_count',10))->get();
         return $this->renderSkin([
             'Sites' => $Sites,
+            'target_instance' => array_get($this->config, 'target_instance',10),
             'title' => $title
         ]);
     }
 
     public function renderSetting(array $args = [])
     {
+        $instance_list = \DB::table('instance_route')->where('site_key', '=', \XeSite::getCurrentSiteKey())->where('module','module/multisite@sitelist')->get();
+        $options = array();
+        foreach($instance_list as $instance){
+            $options[$instance->instance_id] = $instance->url;
+        }
         // 출력할 사이트 개수
-        return uio('formText', ['name'=>'latest_count', 'value'=>array_get($args, 'latest_count'), 'label'=>'출력할 개수', 'description'=>'출력할 사이트 개수를 입력합니다.']);
+        $settings = '';
+        $settings .= uio('formSelect', ['name'=>'target_instance', 'value'=>array_get($args, 'target_instance'), 'options' => $options, 'label'=>'대상 인스턴스', 'description'=>'링크 대상이되는 인스턴스를 선택합니다.']);
+        $settings .= uio('formText', ['name'=>'latest_count', 'value'=>array_get($args, 'latest_count'), 'label'=>'출력할 개수', 'description'=>'출력할 사이트 개수를 입력합니다.']);
+
+        return $settings;
     }
 
     public function resolveSetting(array $inputs = [])
